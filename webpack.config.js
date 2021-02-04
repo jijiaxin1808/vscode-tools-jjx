766e1992b835ef0aebef2daaffcfa14221cd391c
潘神1808 <44405174+jijiaxin1808@users.noreply.github.com>
2021-02-04 19:30:36 +0800

    feat: (webpack)webpack现在可以支持monorepo形式的打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,57 @@
 /* eslint-disable no-undef */
 
 const path = require('path');
+const fs = require('fs');
 
-const config = {
-    target: 'node',
-    entry: './packages/extension/src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'out'),
-        filename: 'extension.js',
-        libraryTarget: 'commonjs2',
-        devtoolModuleFilenameTemplate: '../[resource-path]'
-    },
-    devtool: 'source-map',
-    externals: {
-        vscode: 'commonjs vscode'
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'ts-loader'
-                    }
-                ]
-            }
-        ]
-    },
-    mode: 'development',
-    optimization: {
-        usedExports: true,
-    },
-};
-module.exports = config;
+function scanPackage() {
+    const packages = fs.readdirSync('packages').filter( path =>
+        fs.statSync(`packages/${path}`).isDirectory()
+    );
+    const conf = packages.map(path => ({
+        entry: `./packages/${path}/src/index.ts`,
+        path,
+        filename:`${path}.js`,
+        outpath: `./packages/${path}/dist`
+    }));
+    return conf;
+}
+
+function getConfigs() {
+    return scanPackage().map(config => ({
+        target: 'node',
+        entry:  config.entry,
+        output: {
+            path: path.resolve(__dirname, config.outpath),
+            filename: config.filename,
+            libraryTarget: 'commonjs2',
+            devtoolModuleFilenameTemplate: '../[resource-path]'
+        },
+        devtool: 'source-map',
+        externals: {
+            vscode: 'commonjs vscode'
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'ts-loader'
+                        }
+                    ]
+                }
+            ]
+        },
+        mode: 'development',
+        optimization: {
+            usedExports: true,
+        },
+
+    }));
+}
+
+module.exports = getConfigs();

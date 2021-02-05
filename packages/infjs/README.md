278966e5265c6867687afa573c889a56384162eb
潘神1808 <44405174+jijiaxin1808@users.noreply.github.com>
2021-02-05 11:03:58 +0800

    refactor: (infjs)改为git submodule形式

--- packages/infjs/README.md
+++
@@ -1,6 +0,0 @@
-# Inquirerjs-for-vscode
-Inquirerjs for vscode
-
-this repo provides functions just like Inquirerjs on vscode ui
-
-todo: read doc
--- packages/infjs/src/utils/curry.ts
+++
@@ -1,3 +0,0 @@
-export function createValiteFunc<T, K, U>(fn: (input: T, result: K) => U, result: K ) {
-  return (input: T) => fn(input, result);
-}

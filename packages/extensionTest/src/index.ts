e7d9e29b6beb19d5ec3519a055c8417e0c979e4a
潘神1808 <44405174+jijiaxin1808@users.noreply.github.com>
2021-02-07 21:15:37 +0800

    revert: (extTest)情况测试commnd

--- packages/extensionTest/src/index.ts
+++ packages/extensionTest/src/index.ts
@@ -1,30 +1,3 @@
-import { infInput } from 'infquery';
-import * as vscode from 'vscode';
 export function test() {
-    infInput([
-        {
-            type: "input",
-            name: 'jjxsb',
-            message: '这里是第一个input',
-            // prefix: 'prefix',
-            // suffix: 'subfix'
-        },
-        {
-            type: "input",
-            name: 'aaa',
-            when: (obj)=>{
-                return obj['jjxsb'] === 'jjxsb';
-            },
-            validate: (input, res)=>{
-                console.log({res});
-                if(input.startsWith('jjx')) return `${input} can\`t valite`;
-                return null;
-            },
-            default: (res)=>{
-                return res['jjxsb'];
-            }
-        }
-    ]).then(res=> {
-        vscode.window.showInformationMessage(JSON.stringify(res));
-    });
+   
 }
